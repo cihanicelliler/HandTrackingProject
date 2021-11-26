b4274e9f2b94b423623b200db5bdd2990c345b6f
cihan.icelliler <cihan.icelliler@siemens.com>
2021-11-26 11:30:35 +0300

    Project Finished.

--- VolumeHandControl.py
+++ VolumeHandControl.py
@@ -3,6 +3,9 @@ import time
 import numpy as np
 import HandTrackingModule as htm
 import math
+from ctypes import cast, POINTER
+from comtypes import CLSCTX_ALL
+from pycaw.pycaw import AudioUtilities, IAudioEndpointVolume
 
 wCam, hCam = 640, 480
 
@@ -13,6 +16,18 @@ pTime = 0
 
 detector = htm.handDetector(detectionCon=0.7)
 
+devices = AudioUtilities.GetSpeakers()
+interface = devices.Activate(
+    IAudioEndpointVolume._iid_, CLSCTX_ALL, None)
+volume = cast(interface, POINTER(IAudioEndpointVolume))
+# volume.GetMute()
+# volume.GetMasterVolumeLevel()
+volRange = volume.GetVolumeRange()
+minVol = volRange[0]
+maxVol = volRange[1]
+vol=0
+volBar=400
+volPer=0
 while True:
     success, img = cap.read()
     img = detector.findHands(img)
@@ -20,17 +35,25 @@ while True:
     if len(lmList) != 0:
         x1, y1 = lmList[4][1], lmList[4][2]
         x2, y2 = lmList[8][1], lmList[8][2]
-        cx,cy=(x1+x2)//2,(y1+y2)//2
+        cx, cy = (x1+x2)//2, (y1+y2)//2
 
         cv2.circle(img, (x1, y1), 10, (0, 255, 0), cv2.FILLED)
         cv2.circle(img, (x2, y2), 10, (0, 255, 0), cv2.FILLED)
-        cv2.line(img,(x1,y1),(x2, y2),(0,255,0),3)
+        cv2.line(img, (x1, y1), (x2, y2), (0, 255, 0), 3)
         cv2.circle(img, (cx, cy), 10, (0, 255, 0), cv2.FILLED)
 
-        length=math.hypot(x2-x1,y2-y1)
-        print(length)
-        if length<20:
-            cv2.circle(cv2.circle(img, (cx, cy), 10, (0, 255, 0), cv2.FILLED))
+        length = math.hypot(x2-x1, y2-y1)
+        vol = np.interp(length,[25,250],[minVol,maxVol])
+        volBar = np.interp(length,[25,250],[400,150])
+        volPer = np.interp(length,[25,250],[0,100])
+        volume.SetMasterVolumeLevel(vol, None)
+        if length < 25:
+            cv2.circle(img, (cx, cy), 10, (255, 0, 0), cv2.FILLED)
+
+        cv2.rectangle(img, (50,150),(85,400),(0,255,0),3)
+        cv2.rectangle(img, (50,int(volBar)),(85,400),(0,255,0), cv2.FILLED)
+        cv2.putText(img, f'FPS:{int(volPer)} %', (40, 450),
+                cv2.FONT_HERSHEY_PLAIN, 3, (0, 255, 0), 3)
 
     cTime = time.time()
     fps = 1/(cTime-pTime)
